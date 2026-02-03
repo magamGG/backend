72402f9b1747aafeec43ad0605d3951ac259e546
cheonsol <wjdwlsgur01@gmail.com>
2026-02-03 19:37:27 +0900

    fix : 4차 수정

--- src/main/java/com/kh/magamGG/domain/project/repository/ProjectRepository.java
+++ src/main/java/com/kh/magamGG/domain/project/repository/ProjectRepository.java
@@ -10,15 +10,6 @@ import java.util.List;
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     /**
-     * 프로젝트별 에이전시 소속 작가(웹툰/웹소설) 수
-     */
-    @Query("SELECT COUNT(DISTINCT pm.projectMemberNo) FROM ProjectMember pm " +
-           "WHERE pm.project.projectNo = :projectNo " +
-           "AND pm.member.agency.agencyNo = :agencyNo " +
-           "AND (pm.member.memberRole = '웹툰 작가' OR pm.member.memberRole = '웹소설 작가')")
-    long countArtistsByProjectAndAgency(@Param("projectNo") Long projectNo, @Param("agencyNo") Long agencyNo);
-
-    /**
      * 에이전시 소속 회원이 참여한 진행 중(연재) 프로젝트 목록
      */
     @Query("SELECT DISTINCT p FROM Project p " +
